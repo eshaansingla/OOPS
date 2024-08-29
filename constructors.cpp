7fca5bf88b8e2ba100804250acc753cd82acd6ee
eshaansingla <147623220+eshaansingla@users.noreply.github.com>
2024-08-29 16:48:32 +0530

    copy constructor

--- constructors.cpp
+++ constructors.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 class Car{
     public:
+    Car(){
+
+    }
         int price;
         string name;
         float maxspeed;
@@ -18,6 +21,18 @@ void print(){
 };
 int main(){
     Car c1(10000000,"Bugatti",250.8569,"Eshaan");
+    Car c2;
+    c2.price=1000;
+    c2.name="Swift";
+    c2.maxspeed=100.67095;
+    c2.owner="Someone";
+    Car c3=c2; //deep copy
+    Car c4(c2); // deep copy
+    c3.owner="Anyone";
+    c4.name="BMW";
     c1.print();
+    c2.print();
+    c3.print();
+    c4.print();
 
 }
