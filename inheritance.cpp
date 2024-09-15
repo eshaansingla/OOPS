4f87d39744633b6384093b6ff4fa929f343ae974
eshaansingla <147623220+eshaansingla@users.noreply.github.com>
2024-09-15 16:49:43 +0530

    done

--- inheritance.cpp
+++ inheritance.cpp
@@ -7,9 +7,13 @@ class Vehicle{
         int age;
         float speed;
         Vehicle(){
-
+            cout<<"VEHICLEcons"<<endl;
+    }
+     ~Vehicle(){
+cout<<"VEHICLE"<<endl;
     }
     Vehicle(int price,string fuel,int age,float speed){
+        cout<<"VEHICLEcons"<<endl;
         this->price=price;
         this->fuel=fuel;
         this->age=age;
@@ -21,22 +25,30 @@ class Twowheeler: public Vehicle{
     public:
         int bootspace;
         Twowheeler(){
-
+    cout<<"2wheelerCONS"<<endl;
     }
     Twowheeler(int price,string fuel,int age,float speed,int bootspace): Vehicle(price,fuel,age,speed) {
             this->bootspace = bootspace;
+            cout<<"2wheelercons"<<endl;
         }
+    ~Twowheeler(){
+        cout<<"2wheeler"<<endl;
+    }
 };
 class Bike: public Twowheeler{
     
     public:
         int gears;
     Bike(){
-
+          cout<<"BikC"<<endl;
+    }
+    ~Bike(){
+        cout<<"Bike"<<endl;
     }
     Bike(int price,string fuel,int age,float speed,int bootspace,int gears)
         : Twowheeler(price,fuel,age,speed,bootspace) {
             this->gears = gears;
+            cout<<"Bike"<<endl;
         }
 };
 int main(){
